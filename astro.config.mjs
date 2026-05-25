67f324e59403f9dc383605483b7b47fa1e9c551a
pitchu213-sys <pitchu213@gmail.com>
2026-05-25 10:27:15 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap'; // أضفنا حزمة خريطة الموقع
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  // 1. أضف رابط موقعك الأساسي هنا (بدون شرطة مائلة في النهاية)
+  site: 'https://ahla-qisas.vercel.app', 
+  
+  // 2. أضف sitemap() إلى الإضافات
+  integrations: [mdx(), sitemap()] 
 });
