400a1ac6a789aab3380f57f69f813d1fd42e2b02
pitchu213-sys <pitchu213@gmail.com>
2026-07-13 00:46:46 +0100

    robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   // هذا السطر مهم جداً لأكواد SEO، لا تحذفه
   site: 'https://ahla-qisas.vercel.app', 
   // أزلنا Sitemap مؤقتاً لحل مشكلة البناء
-  integrations: [mdx()] 
+  integrations: [
+    mdx(),
+    sitemap(),
+  ],
 });
